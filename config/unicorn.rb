df1c3f0da7c8493959ae5e4132edd1f54c6aad37
poppinyunhai <yiranyunhaisrh@gmail.com>
2013-07-19 11:05:10 +0800

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deploy/sites/btc/"
+root = "/home/deploy/sites/btc_four/"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.btc.sock"
+listen "/tmp/unicorn.btc_four.sock"
 worker_processes 2
 timeout 30
